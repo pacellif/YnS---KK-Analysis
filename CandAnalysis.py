ea1ee08834b2a184f33439f2bf49f079e44445d9
Pacio <francesco.pacelli@studio.unibo.it>
2023-12-03 19:05:32 +0100

    add features to menu, optimized 3. and 4.

--- CandAnalysis.py
+++ CandAnalysis.py
@@ -1,9 +1,12 @@
+print("\n^^^^^ Y(2S)->µµ + Phi->KK SPECTRUM ANALYSER ^^^^^\n\nImporting modules...")
+
 import ROOT 
-import uproot
-import pandas as pd
+from time import time
 import os
-import time
-import sys
+from sys import argv
+from definitions import CandTreeDefinitions
+import declarations
+
 ROOT.gROOT.SetBatch(True)
 
 
@@ -26,94 +29,50 @@ with open('Y2SPhiRun2List.txt') as f:
 for i in range(len(allFiles)):			
     allFiles[i] = allFiles[i].replace("\n", "")
 
-#	COMPUTING FUNCTIONS
-ROOT.gInterpreter.Declare('''
-Double_t ComputePseudoRapidity(Double_t px, Double_t py, Double_t pz, Double_t ene) {
-    const ROOT::Math::PxPyPzE4D p(px, py, pz, ene);
-    return p.Eta();
-}
-Double_t ComputePhi(Double_t px, Double_t py, Double_t pz, Double_t ene) {
-    const ROOT::Math::PxPyPzE4D p(px, py, pz, ene);
-    return p.Phi();
-}
-Double_t ComputeRapidity(Double_t px, Double_t py, Double_t pz, Double_t ene) {
-    return 0.5 * log( (ene+pz)/(ene-pz) );
-}
-''')
-
-ROOT.gInterpreter.Declare('''
-Double_t ComputeMuMuPiPiInvMass(Double_t px1, Double_t py1, Double_t pz1, 
-                                    Double_t px2, Double_t py2, Double_t pz2,
-                                    Double_t px3, Double_t py3, Double_t pz3,
-                                    Double_t px4, Double_t py4, Double_t pz4) {
-    const Double_t muonMass = 0.105658;
-    const Double_t pionMass = 0.139570;
-    const ROOT::Math::PxPyPzMVector mu1(px1, py1, pz1, muonMass);
-    const ROOT::Math::PxPyPzMVector mu2(px2, py2, pz2, muonMass);
-    const ROOT::Math::PxPyPzMVector pi1(px3, py3, pz3, pionMass);
-    const ROOT::Math::PxPyPzMVector pi2(px4, py4, pz4, pionMass);
-    return (mu1+mu2+pi1+pi2).mass();
-}
-Double_t ComputePiPiInvMass(Double_t px3, Double_t py3, Double_t pz3,
-                     Double_t px4, Double_t py4, Double_t pz4) {
-    const Double_t pionMass = 0.139570;
-    const ROOT::Math::PxPyPzMVector pi1(px3, py3, pz3, pionMass);
-    const ROOT::Math::PxPyPzMVector pi2(px4, py4, pz4, pionMass);
-    return (pi1+pi2).mass();
-}
-''')
-
 #	TREES READING - using command line arguments
+#	- no command line arguments: analysis of all data and store plots in a "test" directory
+# 	- argv is an integer: analysis of first n root files, in a "test" directory
+#	- argv is a string: analysis of all data and store plots in directory named <argv_value>
+
+
+d = "test"
 try: 
-	sample = allFiles[:int(sys.argv[1])] 
+	sample = allFiles[:int(argv[1])] 
 except IndexError:
 	sample = allFiles[:]
+except ValueError:
+	sample = allFiles[:]
+	d = argv[1]
+											
+if not os.path.isdir(f"./{d}/"):			
+	os.system(f"mkdir {d}")							
 
-dataY2SKK = ROOT.RDataFrame("rootuple/CandidateTree",sample)
-if not dataY2SKK:
-	print("Connect hard disk")
-	exit()
+# create a root file to store and edit plots, if necessary											
+p = ROOT.TFile.Open(d+"/h.root","RECREATE")		
+												
 
-################################################################
-#	CANDIDATES KK DECAY	########################################
-################################################################
+# creating datasets for the analysis		
+print("Creating Dataset...")
 
-
-#	DEFINITIONS
-dataY2SKK = dataY2SKK\
-.Define("ditrack_mass", "sqrt(ditrack_p4_fE*ditrack_p4_fE - ditrack_p4_fX*ditrack_p4_fX - ditrack_p4_fY*ditrack_p4_fY - ditrack_p4_fZ*ditrack_p4_fZ)")\
-.Define("dimuon_mass", "sqrt(dimuon_p4_fE*dimuon_p4_fE - dimuon_p4_fX*dimuon_p4_fX - dimuon_p4_fY*dimuon_p4_fY - dimuon_p4_fZ*dimuon_p4_fZ)")\
-.Define("track1_pT", "sqrt(track1_p4_fX*track1_p4_fX + track1_p4_fY*track1_p4_fY)")\
-.Define("track1_eta", "ComputePseudoRapidity(track1_p4_fX, track1_p4_fY, track1_p4_fZ, track1_p4_fE)")\
-.Define("track2_pT", "sqrt(track2_p4_fX*track2_p4_fX + track2_p4_fY*track2_p4_fY)")\
-.Define("track2_eta", "ComputePseudoRapidity(track2_p4_fX, track2_p4_fY, track2_p4_fZ, track2_p4_fE)")\
-.Define("ditrack_pT", "sqrt(ditrack_p4_fX*ditrack_p4_fX + ditrack_p4_fY*ditrack_p4_fY)")\
-.Define("ditrack_eta", "ComputePseudoRapidity(ditrack_p4_fX, ditrack_p4_fY, ditrack_p4_fZ, ditrack_p4_fE)")\
-.Define("dimuon_pT", "sqrt(dimuon_p4_fX*dimuon_p4_fX + dimuon_p4_fY*dimuon_p4_fY)")\
-.Define("dimuon_eta", "ComputePseudoRapidity(dimuon_p4_fX, dimuon_p4_fY, dimuon_p4_fZ, dimuon_p4_fE)")\
-.Define("candidate_pT", "sqrt(candidate_p4_fX*candidate_p4_fX + candidate_p4_fY*candidate_p4_fY)")\
-.Define("candidate_eta", "ComputePseudoRapidity(candidate_p4_fX, candidate_p4_fY, candidate_p4_fZ, candidate_p4_fE)")\
-.Define("trackL_pT", "max(track1_pT, track2_pT)")\
-.Define("trackS_pT", "min(track1_pT, track2_pT)")\
-.Define("mumupipi_mass", "ComputeMuMuPiPiInvMass(muonp_p4_fX, muonp_p4_fY, muonp_p4_fZ, muonn_p4_fX, muonn_p4_fY, muonn_p4_fZ, track1_p4_fX, track1_p4_fY, track1_p4_fZ, track2_p4_fX, track2_p4_fY, track2_p4_fZ)")\
-.Define("pipi_mass", "ComputePiPiInvMass(track1_p4_fX, track1_p4_fY, track1_p4_fZ, track2_p4_fX, track2_p4_fY, track2_p4_fZ)")
+dataY2SKK = CandTreeDefinitions\
+(ROOT.RDataFrame("rootuple/CandidateTree",sample))#\
+#.Define("mumupipi_mcmass", "ComputeMuMuPiPiInvMassMC(muonp_p4_fX, muonp_p4_fY, muonp_p4_fZ, muonn_p4_fX, muonn_p4_fY, muonn_p4_fZ, track1_p4_fX, track1_p4_fY, track1_p4_fZ, track2_p4_fX, track2_p4_fY, track2_p4_fZ)")	#Mass constraint
 
 
-#############################################
-											#
-d="time_test"								#
-if not os.path.isdir(f"./{d}/"):			#
-	os.system(f"mkdir {d}")					#
-# ROOT FILE									#
-p = ROOT.TFile.Open(d+"/h.root","RECREATE")	#
-											#
-#############################################
+#KK filters
+dataKKrs = dataY2SKK.Filter("track1_charge * track2_charge < 0")
+dataKKws = dataY2SKK.Filter("track1_charge * track2_charge > 0")
+
+#############################################################
+#					CANDIDATES KK DECAY						#
+#############################################################
 
 
+#	DEFINING DIFFERENT PLOTS
 """
 #	MASS PLOT OF CANDIDATE	##############################
 	# Start timer
-start = time.time()
+start = time()
 
 YKKMass = ROOT.RooRealVar("YKKMass", "YKKMass", 11, 13.5)
 YKKMass.setBins(1000)
@@ -133,10 +92,6 @@ hist = dataY2SKK.Histo1D(("Dimuon Mass", "Candidate Mass; m[GeV/c^{2}];Counts", 
 cprint(hist, "phikk_plots/dimuonMass", stats=True)
 """
 
-#KK filters
-dataKKrs = dataY2SKK.Filter("track1_charge * track2_charge < 0")
-dataKKws = dataY2SKK.Filter("track1_charge * track2_charge > 0")
-
 #	LEADING KAON PT PLOT
 
 def kaonL_pt ():
@@ -151,34 +106,26 @@ def kaonS_pt():
 #	KK INVARIANT MASS PLOT, WITH CUTS	#################
 def m_kk():
 		# Start timer
-	start = time.time()
+	start = time()
 
 		#borders definitions
-	maxValues = [0.6, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 1.0]
-	minValues = [0.5, 0.5, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8]
+	LValues = [0., 0.6, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 1.0]
+	SValues = [0., 0.5, 0.5, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8]
 		
-		#cuts definitions
-	cuts = ["track1_charge * track2_charge < 0"]
-
-	for i in range(len(maxValues)):
-		    cuts.append(cuts[0] +" & trackL_pT > " + str(maxValues[i]) + " & trackS_pT > " + str(minValues[i]))
-		  
 		#create array of histograms with all cuts  
 	hists = []
-
-	for cut in cuts:
-		hists.append(dataY2SKK.Filter(cut).Histo1D(("KK invariant mass", "#phi #rightarrow K^{+}K^{-};m(KK) [GeV];Counts", 100, 0.99, 1.06), "ditrack_mass"))
-
-
-	a = [0., 0.6, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 1.0]
-	b = [0., 0.5, 0.5, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8]
+	
+	for i,j in zip(LValues, SValues):
+		hists.append(dataKKrs\
+		.Filter(f"trackL_pT > {i} & trackS_pT > {j}")\
+		.Histo1D(("KK invariant mass", "#phi #rightarrow K^{+}K^{-};m(KK) [GeV];Counts", 100, 0.99, 1.06), "ditrack_mass"))
+		
 
 	c0 = ROOT.TCanvas()
 
 	legend = ROOT.TLegend(0.46, 0.15, 0.89, 0.50)
 
 	hists[0].SetStats(0)	#no stats
-	print("type of hists: ", type(hists[0]))
 
 		#overlap plots with cuts
 	for i, hist in enumerate(hists):
@@ -186,7 +133,7 @@ def m_kk():
 		hist.Scale(1./hist.Integral())	#normalization of plots
 		hist.GetYaxis().SetRangeUser(0, 0.015)	#range of y axis
 		hist.SetLineColor(i+1)					
-		legend.AddEntry(hist.GetPtr(), "p_{T} lead > " + str(a[i]) + "; p_{T} soft > " + str(b[i]), "l")	
+		legend.AddEntry(hist.GetPtr(), "p_{T} lead > "+str(LValues[i])+"; p_{T} soft > "+str(SValues[i]), "l")	
 		hist.Draw("same")	#non si distinguono molto bene i dati
 		
 	legend.Draw("")
@@ -201,9 +148,11 @@ def m_kk():
 
 #	FIT OF KK_PT CUT	#####################
 #	take the cut pt_kL > 1.2 and pt_kS > 1.0
-def m_kk_fit():
-	dfkk = dataKKrs.Filter("trackL_pT > 1.2 & trackS_pT > 1.0")\
-	.Filter("ditrack_mass > 1.0 & ditrack_mass < 1.04").AsNumpy(columns=["ditrack_mass"])
+
+def m_kk_fit(ptL = 1.2, ptS = 1.0):
+	dfkk = dataKKrs.Filter(f"trackL_pT > {ptL} & trackS_pT > {ptS}")\
+	.Filter("ditrack_mass > 1.0 & ditrack_mass < 1.04")\
+	.AsNumpy(columns=["ditrack_mass"])
 		
 		#variable
 	kkmass = ROOT.RooRealVar("ditrack_mass", "m(KK) [GeV]", 1.00, 1.04)
@@ -215,8 +164,8 @@ def m_kk_fit():
 	
 		#model
 	mean = ROOT.RooRealVar("#mu_{#phi}", "mean of gaussian", 1.019, 1.018, 1.020) #mean value, min value, max value
-	sigma = ROOT.RooRealVar("#sigma_{#phi}", "resolution", 0.00125, 0.001, 0.002) # fixed res from MC
-	sigma.setConstant(1)
+	sigma = ROOT.RooRealVar("#sigma_{#phi}", "resolution", 0.00125, 0.001, 0.002) 
+	sigma.setConstant(1)		# fixed res from MC????????
 	width = ROOT.RooRealVar("#Gamma_{#phi}", "width", 0.00439, 0.003, 0.006)
 
 	f0 = ROOT.RooRealVar("f0", "f0", +0.2, -1, +1)
@@ -261,7 +210,7 @@ def m_kk_fit():
 	os.system(f"xdg-open {d}/PhiMassPlot.pdf")
 		
 		# End timer
-	end = time.time()
+	end = time()
 
 		# Calculate elapsed time
 	elapsed = end - start
@@ -282,11 +231,7 @@ ditrack_mass > 1.0150 &
 ditrack_mass < 1.0239 &
 trackL_pT > 1.0 & 
 trackS_pT > 0.8'''
-phiKKwsSelection = '''candidate_vProb > 0.1 &
-ditrack_mass > 1.0150 & 
-ditrack_mass < 1.0239 &
-trackL_pT > 1.0 & 
-trackS_pT > 0.8'''
+
 
 # ditrack mass: interval of phi rest mass
 # vProb: vertex probability
@@ -301,9 +246,9 @@ tagli = tagli.replace(" & ", "\n")
 os.system(f"echo \"{tagli}\nbinning: {binning}\" > {d}/tagli.txt")
 
 #histograms
-def phi_cand(zoom = False):
+def mumukk(zoom = False):
 	hist0 = dataKKrs.Filter(ymumu_filter + phiKKSelection).Histo1D(("MuMuKK cands", "Y(2S)(#rightarrow #mu^{+}#mu^{-})#phi(#rightarrow K^{+}K^{-});m(#mu#muKK) - m(#mu#mu) + m^{PDG}(Y) [GeV];Counts", binning, edge[0], edge[1]), "candidate_vMass")
-	hist1 = dataKKws.Filter(ymumu_filter + phiKKwsSelection).Histo1D(("MuMuKK cands", "Y(2S)(#rightarrow #mu^{+}#mu^{-})#phi(#rightarrow K^{+}K^{-});m(#mu#muKK) - m(#mu#mu) + m^{PDG}(Y) [GeV];Counts", binning, edge[0], edge[1]), "candidate_vMass")
+	hist1 = dataKKws.Filter(ymumu_filter + phiKKSelection).Histo1D(("MuMuKK cands", "Y(2S)(#rightarrow #mu^{+}#mu^{-})#phi(#rightarrow K^{+}K^{-});m(#mu#muKK) - m(#mu#mu) + m^{PDG}(Y) [GeV];Counts", binning, edge[0], edge[1]), "candidate_vMass")
 
 	c0 = ROOT.TCanvas()
 
@@ -329,8 +274,8 @@ def phi_cand(zoom = False):
 	p.WriteObject(c0,"phi_candidate")
 	
 	if zoom:
-		hist0.GetXaxis().SetRangeUser(11., 11.6)
-		hist1.GetXaxis().SetRangeUser(11., 11.6)
+		hist0.GetXaxis().SetRangeUser(11.3, 11.6)
+		hist1.GetXaxis().SetRangeUser(11.3, 11.6)
 		
 		hist2 = hist0
 		hist3 = hist1
@@ -344,22 +289,52 @@ def phi_cand(zoom = False):
 		c0.Draw("")
 		c0.SaveAs(d+"/PhiCandidateZoom.pdf")
 		os.system(f"xdg-open {d}/PhiCandidateZoom.pdf")
+"""
+def mumupipi():
 
+	mass_constraint_selection = ymumu_filter+phiKKSelection+ " &  mumupipi_mcmass > 11. & mumupipi_mcmass < 11.4"
 
+	hist0 = dataKKrs.Filter(ymumu_filter + phiKKSelection).Histo1D(("MuMuPiPi cands", "Y(2S)(#rightarrow #mu^{+}#mu^{-})#phi(#rightarrow #pi^{+}#pi^{-});m(#mu#mu#pi#pi) - m(#mu#mu) + m^{PDG}(Y) [GeV];Counts", 200, edge[0], edge[1]), "candidate_vMass")
+	hist1 = dataKKws.Filter(ymumu_filter + phiKKwsSelection).Histo1D(("MuMuPiPi cands", "Y(2S)(#rightarrow #mu^{+}#mu^{-})#phi(#rightarrow #pi^{+}#pi^{-});m(#mu#mu#pi#pi) - m(#mu#mu) + m^{PDG}(Y) [GeV];Counts", 200, edge[0], edge[1]), "candidate_vMass")
+
+	c0 = ROOT.TCanvas()
+
+	hist0.SetStats(0)
+
+	hist0.SetLineColor(1)
+	hist1.SetLineColor(2)
+
+	legend = ROOT.TLegend(0.7, 0.1, 0.89, 0.3) #(xmin,ymin,xmax,ymax)
+
+	legend.AddEntry(hist0.GetPtr(), "RS pions", "l")
+	legend.AddEntry(hist1.GetPtr(), "WS pions (norm)", "l")
+
+	hist1.Scale(hist0.Integral()/hist1.Integral())
+	        
+	hist0.Draw("")
+	hist1.Draw("same")
+	legend.Draw("")
+	c0.Draw("")
+	c0.SaveAs(d+"/Phi_pipi.pdf")
+	os.system(f"xdg-open {d}/Phi_pipi.pdf")
+	
+	p.WriteObject(c0,"phi_pipi")
+"""
+	
 #	MENU
-lang = input("Select plots (Separate by spacing):\n1. Leading Kaon pt\n2. Soft Kaon pt\n3. KK invariant mass (with K_pt cuts)\n4. Fit KK invariant mass\n5. Phi Candidate plot\n6. Phi candidate plot with Zoom\nENTER: 1-5 Plots\nPress \"q\" to EXIT.\n").split()
+lang = input("\nSelect plots (Separate by spacing):\n1. Leading Kaon pt\n2. Soft Kaon pt\n3. KK invariant mass (with K_pt cuts)\n4. Fit KK invariant mass\n5. Phi Candidate plot\n6. Phi candidate plot with Zoom\nENTER: 1-5 Plots\nPress \"q\" to EXIT.\n").split()
 
 print("Processing...")
 
 	# Start timer
-start = time.time()
+start = time()
 if not lang:
 #print all plots
 		kaonL_pt()
 		kaonS_pt()
 		m_kk()					
 		m_kk_fit()				#save to .root
-		phi_cand()				#save to .root
+		mumukk()				#save to .root
 else:
 	for i in lang:
 		match i:
@@ -372,9 +347,9 @@ else:
 			case "4":
 				m_kk_fit()		#save to .root
 			case "5":
-				phi_cand()		#save to .root
-			case "6":			
-				phi_cand(zoom=True)
+				mumukk()		#save to .root
+			case "6":		
+				mumukk(zoom=True)
 			case "q":
 				print ("Bye Bye")
 				exit()
@@ -384,7 +359,7 @@ else:
 
 
 	# End timer
-end = time.time()
+end = time()
 
 	# Calculate elapsed time
 elapsed = end - start
